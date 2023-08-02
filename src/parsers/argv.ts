6cc5a1989bf5cb6b7d8fb7bf2f71a69c5dac2b48
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-08-02 19:00:50 +0300

    feat: improved argv parsing

--- src/parsers/argv.ts
+++ src/parsers/argv.ts
@@ -3,18 +3,55 @@ import type { IArgs } from "../types";
 export const parseArgv = () => {
   const args = process.argv.slice(2);
   let lastFlag = "";
-  const parsedArgs = args.reduce<Record<string, string[]>>((acc, str) => {
-    if (str.startsWith("--")) {
-      // NOTE: this is a key
-      lastFlag = str.slice(2).toLowerCase();
-      return acc;
-    } else {
+  const parsedArgs = args.reduce<Record<string, string | boolean | string[]>>(
+    (acc, str) => {
+      if (str.startsWith("--")) {
+        // NOTE: this is a full key
+        const clearedFlag = str.slice(2).toLowerCase();
+        if (clearedFlag.includes("=")) {
+          const [flag, val] = clearedFlag.split("=");
+          lastFlag = flag;
+          return { ...acc, [lastFlag]: val };
+        }
+        lastFlag = clearedFlag;
+        return { ...acc, [lastFlag]: true };
+      }
+
+      if (str.startsWith("-")) {
+        // NOTE: this is a short key(s)
+        lastFlag = str[str.length - 1];
+        const flags = str
+          .slice(1)
+          .split("")
+          .reduce<Record<string, boolean>>(
+            (keys, shortKey) => ({ ...keys, [shortKey]: true }),
+            {},
+          );
+        return { ...acc, ...flags };
+      }
+
+      // NOTE: this is a value
+      if (lastFlag in acc) {
+        // NOTE: this is array of values
+        const prevVal = acc[lastFlag];
+        if (Array.isArray(prevVal)) {
+          return { ...acc, [lastFlag]: [...prevVal, str] };
+        }
+        if (typeof prevVal === "boolean") {
+          return { ...acc, [lastFlag]: str };
+        }
+        return {
+          ...acc,
+          [lastFlag]: [prevVal, str],
+        };
+      }
       return {
         ...acc,
-        [lastFlag]: lastFlag in acc ? [...acc[lastFlag], str] : [str],
+        [lastFlag]: str,
       };
-    }
-  }, {});
+    },
+    {},
+  );
 
   return parsedArgs as IArgs;
 };
